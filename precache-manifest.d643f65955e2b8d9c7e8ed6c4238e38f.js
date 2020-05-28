86312cede7f3438f9d65064f66ec6636cd9914a8
Jyri Alanko <jyrialanko95@hotmail.com>
2020-05-28 09:20:43 +0300

    Updates

---
+++ precache-manifest.d643f65955e2b8d9c7e8ed6c4238e38f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ee5642a8bfa2709084db1d6b341eca",
+    "url": "/collector/index.html"
+  },
+  {
+    "revision": "d50be5304923f94b5a16",
+    "url": "/collector/static/css/main.9cc2e643.chunk.css"
+  },
+  {
+    "revision": "197be99acb8ee2bdbfb6",
+    "url": "/collector/static/js/2.74b7dd44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/collector/static/js/2.74b7dd44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d50be5304923f94b5a16",
+    "url": "/collector/static/js/main.b57448bc.chunk.js"
+  },
+  {
+    "revision": "3e267be1fb50e5828274",
+    "url": "/collector/static/js/runtime-main.9c7ed302.js"
+  }
+]);
